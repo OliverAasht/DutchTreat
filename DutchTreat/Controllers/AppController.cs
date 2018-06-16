0c82aa843a8ea1241d256df8a388ad9757311aa7
Oli <oliveraasht@gmail.com>
2018-06-16 22:53:15 +0100

    Added more views. Used TagHelper, Razor page, Model Binding and Data Validation

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,39 @@ namespace DutchTreat.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Send the email
+
+            }
+            else
+            {
+                // Show the errors
+            
+            }
+
+            return View();
+
+        }
+
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
             return View();
         }
     }
